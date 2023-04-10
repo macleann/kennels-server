d9b6211c55bd29f8b53cdfee87052945ae56d275
Neil MacLean <nbmac13@gmail.com>
2023-04-10 13:53:44 -0500

    core chapters up to SQL

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location, get_all_employees, get_single_employee, get_all_customers, get_single_customer
+from views import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_all_locations, get_single_location, create_location, delete_location, update_location, get_all_employees, get_single_employee, create_employee, delete_employee, update_employee, get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
 
 
 # Here's a class. It inherits from another class.
@@ -29,17 +29,17 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = get_single_animal(id)
             else:
                 response = get_all_animals()
-        elif self.path == "/locations":
+        elif resource == "locations":
             if id is not None:
                 response = get_single_location(id)
             else:
                 response = get_all_locations()
-        elif self.path == "/employees":
+        elif resource == "employees":
             if id is not None:
                 response = get_single_employee(id)
             else:
                 response = get_all_employees()
-        elif self.path == "/customers":
+        elif resource == "customers":
             if id is not None:
                 response = get_single_customer(id)
             else:
@@ -50,23 +50,79 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Send a JSON formatted string as a response
         self.wfile.write(json.dumps(response).encode())
 
+
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server"""
-
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = {"payload": post_body}
-        self.wfile.write(json.dumps(response).encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new resource
+        new_resource = None
+
+        # Add a new resource to the list.
+        if resource == "animals":
+            new_resource = create_animal(post_body)
+        elif resource == "locations":
+            new_resource = create_location(post_body)
+        elif resource == "employees":
+            new_resource = create_employee(post_body)
+        elif resource == "customers":
+            new_resource = create_customer(post_body)
+
+        # Encode the new resource and send in response
+        self.wfile.write(json.dumps(new_resource).encode())
+
 
     # A method that handles any PUT request.
     def do_PUT(self):
-        """Handles PUT requests to the server"""
-        self.do_PUT()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+        elif resource == "locations":
+            update_location(id, post_body)
+        elif resource == "employees":
+            update_employee(id, post_body)
+        elif resource == "customers":
+            update_customer(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+        
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+        elif resource == "locations":
+            delete_location(id)
+        elif resource == "employees":
+            delete_employee(id)
+        elif resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def _set_headers(self, status):
         # Notice this Docstring also includes information about the arguments passed to the function
